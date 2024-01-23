02b5448e6780b4cd705f0eeeb298a0ac4b559b50
gugmin <149454489+gugmin@users.noreply.github.com>
2024-01-23 15:23:44 +0900

    [Fix] ItemRespawmer
    공통시간 이용해서 정해진시간에 일괄 생성되게 했음

--- Assets/02.Scripts/Environment/ItemRespawner.cs
+++ Assets/02.Scripts/Environment/ItemRespawner.cs
@@ -6,14 +6,23 @@ public class ItemRespawner : MonoBehaviour
 {
     public List<GameObject> ItemWaitSpawnList = new List<GameObject>();
 
-    
-
     private void FixedUpdate()
     {
-        if(GameManager.instance.GlobalTimeManager.DayTime < 0.3f)
+        if (ItemWaitSpawnList.Count > 0)
         {
-
+            ReSpwanItem();
         }
     }
 
+    void ReSpwanItem()
+    {
+        if (0.3f <= GameManager.instance.GlobalTimeManager.DayTime && GameManager.instance.GlobalTimeManager.DayTime < 0.31f)
+        {
+            for (int i = 0; i < ItemWaitSpawnList.Count; i++)
+            {
+                ItemWaitSpawnList[i].SetActive(true);
+            }
+            ItemWaitSpawnList.Clear();
+        }
+    }
 }
