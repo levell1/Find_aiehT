827741dbae432b35373d5aaddab2f9f6dfbbb5a8
gugmin <149454489+gugmin@users.noreply.github.com>
2024-01-23 17:33:38 +0900

    [Feature] ItemObject,Layer
    Playere,Enemy 레이어 추가, 드랍아이템, 채집물 추가

--- Assets/02.Scripts/Environment/ItemObject.cs
+++ Assets/02.Scripts/Environment/ItemObject.cs
@@ -8,11 +8,22 @@ using static UnityEditor.Progress;
 public class ItemObject : MonoBehaviour
 {
     private ItemRespawner _itemRespawner;
+    private Vector3 _itemSpawnPoint;
+    private Rigidbody _itemRigidbody;
+
     public ItemSO ItemData;
 
     private void Awake()
     {
+        _itemRigidbody = GetComponent<Rigidbody>();
         _itemRespawner = GetComponentInParent<ItemRespawner>();
+        _itemSpawnPoint = transform.position;
+    }
+
+    private void OnEnable()
+    {
+        _itemRigidbody.velocity = Vector3.zero;
+        transform.position = _itemSpawnPoint;
     }
 
     public void GetItem()
