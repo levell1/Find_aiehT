fe3cba7cd9c8a8d84764d002e63f9ab988ccb19a
uhbbang3 <ansdnjswjd33@gmail.com>
2024-01-29 00:31:44 +0900

    [Update] Delete orderFood class, List -> Dictionary

--- Assets/02.Scripts/Manager/DataManager.cs
+++ Assets/02.Scripts/Manager/DataManager.cs
@@ -1,27 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class orderFood 
-{
-    public FoodSO foodSO;
-    public int foodCount;
-}
-
 public class DataManager : MonoBehaviour
 {
     public FoodSO[] FoodSoDatas = new FoodSO[20];
-    public List<orderFood> Orders = new List<orderFood>();
+    public Dictionary<FoodSO, int> Orders = new ();
 
     public void AddMenu(FoodSO fooddata, int count) 
     {
-        orderFood orderFood = new orderFood();
-        orderFood.foodSO = fooddata;
-        orderFood.foodCount = count;
-        Orders.Add(orderFood);
+        Orders.Add(fooddata, count);
     }
 
     public void RemoveOrderData() 
     {
-        Orders.RemoveRange(0,Orders.Count);
+        Orders.Clear();
     }
 }
