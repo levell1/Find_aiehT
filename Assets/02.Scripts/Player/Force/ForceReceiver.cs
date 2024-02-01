1d7e017e1263b20e33f5e0ecc54bcfaf93177c2d
junhyeongBak <dj043045@gmail.com>
2024-02-01 16:55:26 +0900

    [Update] Custom Develop Editor
    개발자 에디터 생성 (씬이동, 아이템 골드추가, 레벨업)

--- Assets/02.Scripts/Player/Force/ForceReceiver.cs
+++ Assets/02.Scripts/Player/Force/ForceReceiver.cs
@@ -18,32 +18,28 @@ public class ForceReceiver : MonoBehaviour
         Player = GetComponent<Player>();
     }
 
-    void Update()
-    {
-        forceTime = Time.deltaTime;
-        if (Player.Rigidbody.velocity.y <= 0f && Player.GroundCheck.IsGrounded())
-        {
-            _moveForce = Physics.gravity.y * forceTime;
-        }
-       
-        _impact = Vector3.SmoothDamp(_impact, Vector3.zero, ref _dampingVelocity, _drag);
+    //private void Update()
+    //{
+    //    Debug.Log(Player.Rigidbody.velocity.y);
+    //}
 
-    }
+    //void Update()
+    //{
+    //    forceTime = Time.deltaTime;
+    //    if (Player.Rigidbody.velocity.y <= 0f && Player.GroundCheck.IsGrounded())
+    //    {
+    //        _moveForce = Physics.gravity.y * forceTime;
+    //    }
 
-    public void Reset()
-    {
-        _impact = Vector3.zero;
-    }
-
-    public void AddForce(Vector3 force)
-    {
-        _impact += force;
-    }
+    //    _impact = Vector3.SmoothDamp(_impact, Vector3.zero, ref _dampingVelocity, _drag);
+    //    Player.Rigidbody.AddForce(_impact, ForceMode.Impulse);
+    //}
 
     public void Jump(float jumpForce)
     {
         if(Player.GroundCheck.IsGrounded())
         {
+            //Debug.Log(Player.Rigidbody.velocity.y);
             Player.Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
