cf23309962050b27359bee3cdb70103c9169ea7c
junhyeongBak <dj043045@gmail.com>
2024-02-02 20:42:08 +0900

    [Fix] Spread Skill Delay
    e스킬 스킬 딜레이 맞추기 작업

--- Assets/02.Scripts/Player/Weapon/PlayerSpreadSkill.cs
+++ Assets/02.Scripts/Player/Weapon/PlayerSpreadSkill.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class PlayerSpreadSkill : MonoBehaviour
     [SerializeField] private MeshCollider _skillRangeCollider;
 
     private float _damage;
+    private float _playTime = 0.6f;
 
     private readonly List<Collider> _alreadyCollidedObjects = new List<Collider>();
 
@@ -55,14 +57,31 @@ public class PlayerSpreadSkill : MonoBehaviour
         this._damage = damage + _equipmentDatas.SumDmg * 2;
     }
 
-    // 콜라이더를 활성화할 때 이 메서드 호출
     public void EnableCollider()
     {
+        StartCoroutine(DelayCollider());
+    }
+
+    private IEnumerator DelayCollider()
+    {
+
+        yield return new WaitForSeconds(_playTime);
+
         if (_skillRangeCollider != null)
         {
             _skillRangeCollider.enabled = true;
         }
+
     }
+
+    // 콜라이더를 활성화할 때 이 메서드 호출
+    //public void EnableCollider()
+    //{
+    //    if (_skillRangeCollider != null)
+    //    {
+    //        _skillRangeCollider.enabled = true;
+    //    }
+    //}
 
     // 콜라이더를 비활성화할 때 이 메서드 호출
     public void DisableCollider()
