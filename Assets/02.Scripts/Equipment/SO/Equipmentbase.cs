ee31f87192c0379800b31bd32daf24fdf829fcb2
junhyeongBak <dj043045@gmail.com>
2024-02-06 21:35:13 +0900

    [Fix] add Unique number
    고유식별번호 추가

--- Assets/02.Scripts/Equipment/SO/Equipmentbase.cs
+++ Assets/02.Scripts/Equipment/SO/Equipmentbase.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Equipment", menuName = "Equipment/Default", order = 0)]
 public class EquipmentBase : ScriptableObject
 {
+    public int EquipmentID; 
     public string Name;
     public int UpgradeGold;
     public Sprite EquipSprite;
