fba1a87a8384a970b253424d1507f00fbe6e0108
uhbbang3 <ansdnjswjd33@gmail.com>
2024-02-11 19:15:37 +0900

    [Update] Effect Manager - GameObejct -> Particle System

--- Assets/02.Scripts/Manager/EffectManager.cs
+++ Assets/02.Scripts/Manager/EffectManager.cs
@@ -10,75 +10,73 @@ public class EffectManager : MonoBehaviour
     private WaitForSeconds _playerAttackEffectTime = new WaitForSeconds(0.3f);
 
     private GameObject _player;
-    private GameObject _healingObject;
-    private GameObject _staminaHealingObject;
-    private GameObject _playerAttackObject;
-    private GameObject _footStepObject;
+    private ParticleSystem _healingObject;
+    private ParticleSystem _staminaHealingObject;
+    private ParticleSystem _playerAttackObject;
+    private ParticleSystem _footStepObject;
 
     [Header("Effect Prefabs")]
-    [SerializeField] private GameObject _levelUpEffect;
-    [SerializeField] private GameObject _healingEffect;
-    [SerializeField] private GameObject _staminaHealingEffect;
-    [SerializeField] private GameObject _playerAttackEffect;
-    [SerializeField] private GameObject _footStepEffect;
+    [SerializeField] private ParticleSystem _levelUpEffect;
+    [SerializeField] private ParticleSystem _healingEffect;
+    [SerializeField] private ParticleSystem _staminaHealingEffect;
+    [SerializeField] private ParticleSystem _playerAttackEffect;
+    [SerializeField] private ParticleSystem _footStepEffect;
 
     private void Start()
     {
         _player = GameManager.Instance.Player;
 
         _healingObject = Instantiate(_healingEffect, _player.transform);
-        _healingObject.SetActive(false);
-
         _staminaHealingObject = Instantiate(_staminaHealingEffect, _player.transform);
-        _staminaHealingObject.SetActive(false);
-
         _playerAttackObject = Instantiate(_playerAttackEffect, _weaponPos.transform);
-        _playerAttackObject.SetActive(false);
-
         _footStepObject = Instantiate(_footStepEffect, _player.transform);
-        _footStepObject.SetActive(false);
     }
 
     public void PlayLevelUpEffect()
     {
-        GameObject levelupEffectObject = Instantiate(_levelUpEffect, _player.transform);
+        ParticleSystem levelupEffectObject = Instantiate(_levelUpEffect, _player.transform);
         Destroy(levelupEffectObject, 5f);
     }
     
     public void PlayHealingEffect()
     {
-        _healingObject.SetActive(true);
+        _healingObject.Play();
         StartCoroutine(TurnOffEffect(_healingObject, _healingEffectTime));
     }
 
     public void PlayStaminaEffect()
     {
-        _staminaHealingObject.SetActive(true);
+        _staminaHealingObject.Play();
         StartCoroutine(TurnOffEffect(_staminaHealingObject, _healingEffectTime));
     }
 
     public void PlayAttackEffect()
     {
-        _playerAttackObject.SetActive(true);
+        _playerAttackObject.Play();
         StartCoroutine(TurnOffEffect(_playerAttackObject, _playerAttackEffectTime));
     }
 
     public void PlayFootStepEffect()
     {
-        _footStepObject.SetActive(true);
+        _footStepObject.Play();
     }
 
     public void StopFootStepEffect()
     {
-        _footStepObject.SetActive(false);
+        _footStepObject.Stop();
+    }
+
+    public void CustomerEatFoodEffect()
+    {
+
     }
 
     #region Coroutine
 
-    IEnumerator TurnOffEffect(GameObject effectObject, WaitForSeconds effectTime)
+    IEnumerator TurnOffEffect(ParticleSystem effectObject, WaitForSeconds effectTime)
     {
         yield return effectTime;
-        effectObject.SetActive(false);
+        effectObject.Stop();
     }
 
     #endregion
