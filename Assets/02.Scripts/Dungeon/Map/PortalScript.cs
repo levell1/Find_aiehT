b7fc9a3d1f9c73fe122f339b24af605fd1704b65
종 욱 <whddnr0317@naver.com>
2024-02-27 10:55:53 +0900

    [Fix] 던전 ai 수정, 포탈버그 수정

--- Assets/02.Scripts/Dungeon/Map/PortalScript.cs
+++ Assets/02.Scripts/Dungeon/Map/PortalScript.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PortalScript : MonoBehaviour
 {
     [SerializeField] private DungeonManager _dungeonManager;
+    private CapsuleCollider _capsuleCollider;
 
     private void Awake()
     {
+        _capsuleCollider= GetComponent<CapsuleCollider>();
         _dungeonManager = FindAnyObjectByType<DungeonManager>();
         transform.gameObject.SetActive(false);
     }
@@ -14,6 +16,7 @@ public class PortalScript : MonoBehaviour
         if (other.CompareTag(TagName.Player)) 
         {
             _dungeonManager.GoNextStage();
+            _capsuleCollider.enabled = false;
         }
     }
 
